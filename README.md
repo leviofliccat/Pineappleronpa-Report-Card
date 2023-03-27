cfe7744d0a7b41c269882d6892d61475f59d941d
FelicityNop47681837 <leviosaash13950@gmail.com>
2023-03-27 19:41:10 +1100

    formatter and readme updated
    changed formatter to work with most of will's formatting (but stuff like sprite expression names and scene names need to be checked)

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Pineappleronpa Report Card
- 
+
+Formatting: 
+\ # narration
+\ > choice menu
+\ $$ scene transition
+\ Name: Speech
+\ [NAME: SPRITE]
+
+
+Suggested changes:
+\ ?Name: Thought/blue text
+
+
+If the line contains yellow, red, or green text, it needs to be coloured manually.
+Choices also need to be done manually by prath. Is there an indication of where the choice options end or where it jumps...?
+Suggestion: Put some indication that the line needs to be done manually.
+
+\ @@ line that needs to be reviewed
+
+and then ig when it gets formatted, write it as !!!CHOICE_START or !!!CHECK_FORMATTING
--- game/script_formatter.py
+++ game/script_formatter.py
@@ -1,13 +1,15 @@
 
-## Check use with quotation marks, slashes, etc
-## blue text needs to be changed
+## need to Check use with quotation marks, slashes, etc... still not 100% but pretty solid so far
 
 ## This program successfully:
     # Removes all extra spaces in the script before formatting
     # Removes extra line spaces
     # Understands dialogue as Speaker: Speech format
-            # Understands narration as anything that is not dialogue or blue text format
-            # Currently formats blue text with the colour #2D70D6 and removes the asterisks
+    # Understands narration as starting with #
+    # Currently formats blue text with the colour #2D70D6 and understands it as starting with ?
+    # Preserves lines starting with > and @@ for manual review
+    # Understands [NAME: EXPRESSION]
+    # Understands scene changes as starting with $$
 
     # Exports the entire script, in order, to a .txt document to be copy pasted directly into Ren'Py.
     # Replaces the colons it removes as part of the separating process.
@@ -17,15 +19,11 @@
 ## Before executing this program
 ########################################################################################################
 
-## Ensure the file name of the script to be formatted is correct:
+## Ensure the file name and encoder of the script to be formatted is correct:
 original_script_file_name = "testing_formats_original.txt"
-original_script_encoding = "UTF-8" #with BOM"
+original_script_encoding = "UTF-8" # whatever it says in the bottom right of the txt file
 
-## Ensure the character names are consistent with the script (coloured text within dialogue like important yellow text needs to be done manually): 
-
-list_of_character_names = ["reader", "luke", "david", "sean", "julian", "riley", "mitch", "will", "johan", "mel", "felicity", "blue_text", "narration"]
-
-
+## Ensure the character names are consistent with the script (case doesn't matter): 
 # Create empty lists to be filled with character dialogue. Each line of dialogue will be a seperate element.
 reader = []
 luke = []
@@ -39,16 +37,32 @@ johan = []
 mel = []
 felicity = []
 
+scene_transition = []
+expression_change = []
 blue_text = []
 narration = []
+review = []
 
-## Ensure that the Ren'Py character codes in the game's code are correct here:
-sean_object = "s"
-reader_object = "r"
+## Ensure the desired character object codes to be used in the game's dialogue code are consistent:
+object_codes = {
+    "reader_object" : "r",
+    "luke_object" : "l",
+    "david_object" : "d",
+    "sean_object" : "s",
+    "julian_object" : "j",
+    "riley_object" : "r",
+    "mitch_object" : "m",
+    "will_object" : "w",
 
+    "johan_object" : "o",
+    "mel_object" : "e",
 
-#########################################################################################################
+    "felicity_object" : "f"
+}
 
+##############################################################
+## Document preparation
+##############################################################
 
 # Import functions that replace colons.
 from exp import colons_in_dialogue
@@ -58,6 +72,7 @@ from exp import colons_in_plain_text
 def remove_all_extra_spaces(string):
     return " ".join(string.split())
 
+
 if original_script_encoding == "UTF-8 with BOM":
     script_encoding = "utf-8-sig"
 elif original_script_encoding == "UTF-8":
@@ -66,86 +81,200 @@ elif original_script_encoding == "UTF-8":
 
 # The open() function opens the text file to be read, 
 # then the file is split and turned into a list readable by python.
-script_read = open(original_script_file_name, "r", encoding=script_encoding)            # need to add encoding="utf-8-sig" in the open() command if it's utf-8 with bom
+script_read = open(original_script_file_name, "r", encoding=script_encoding)
 chat_script = script_read.read().split("\n")
 while("" in chat_script):
     chat_script.remove("")
 
 
+##############################################################
+## Script preparation
+##############################################################
+
 # For each line in the script, take the line, turn it into a string, 
 # split up speaker and speech, identify type, then sort into lists.
+
+character_names = ["Reader", "Luke", "David", "Sean", "Julian", "Riley", "Mitch", "Will", "Johan", "Mel", "Felicity"]
+list_character_names = [reader, luke, david, sean, julian, riley, mitch, will, johan, mel, felicity]
 i = 0
-while i < len(chat_script):                                                                 ## This needs to be made more efficient; nested loops
+while i < len(chat_script):                                                    
     values = chat_script[i].split(": ")
+    
+    x = 0
+    
+    while x < len(character_names):
+        person = character_names[x]
+        if values[0] == person:
+            position = character_names.index(person)
+            name = list_character_names[position]
+            name.append(remove_all_extra_spaces(colons_in_dialogue(values)[1]))
+        x=x+1
 
-    if values[0] == "Sean": 
-        # calling the function to put colons back in if they were removed.
-        sean.append(remove_all_extra_spaces(colons_in_dialogue(values)[1]))
-
-    elif values[0] == "Reader":
-        reader.append(remove_all_extra_spaces(colons_in_dialogue(values)[1]))
-
-    else:
-        if chat_script[i].startswith("?") == True:
-            blue_text.append(colons_in_plain_text(values)[0])
-
-        else:
-            narration.append(remove_all_extra_spaces(colons_in_plain_text(values)[0]))
+    if chat_script[i].startswith("?") == True:
+        blue_text.append(colons_in_plain_text(values)[0])
+    elif chat_script[i].startswith(">") == True or chat_script[i].startswith("@@") == True:
+        review.append(remove_all_extra_spaces(colons_in_plain_text(values)[0]))
+    elif chat_script[i].startswith("[") == True:
+        expression_change.append(remove_all_extra_spaces(colons_in_plain_text(values)[0]))
+    elif chat_script[i].startswith("$$") == True:
+        scene_transition.append(remove_all_extra_spaces(colons_in_plain_text(values)[0]))
+    elif chat_script[i].startswith("#"):
+        narration.append(remove_all_extra_spaces(colons_in_plain_text(values)[0]))
+        
     i=i+1
 
 
-# Create empty lists for formatted lines and define character objects.                  Again, make this a loop
-renpy_sean = []
+
+# i = 0
+# while i < len(chat_script):                                                    ## This needs to be made more efficient; nested loops
+#     values = chat_script[i].split(": ")
+
+#     if values[0] == "Sean": 
+#         # calling the function to put colons back in if they were removed.
+#         sean.append(remove_all_extra_spaces(colons_in_dialogue(values)[1]))
+
+#     elif values[0] == "Reader":
+#         reader.append(remove_all_extra_spaces(colons_in_dialogue(values)[1]))
+
+#     else:
+#         if chat_script[i].startswith("?") == True:
+#             blue_text.append(colons_in_plain_text(values)[0])
+
+#         else:
+#             narration.append(remove_all_extra_spaces(colons_in_plain_text(values)[0]))
+#     i=i+1
+
+
+# Create empty lists for formatted lines and define character objects.                  
 
 renpy_reader = []
+renpy_luke = []
+renpy_david = []
+renpy_sean = []
+renpy_julian = []
+renpy_riley = []
+renpy_mitch = []
+renpy_will = []
+renpy_johan = []
+renpy_mel = []
+renpy_felicity = []
 
+renpy_scene_transition = []
+renpy_expression_change = []
 renpy_blue_text = []
-
 renpy_narration = []
+renpy_review = []
 
-
-
-
+list_renpy_names = [renpy_reader, renpy_luke, renpy_david, renpy_sean, renpy_julian, renpy_riley, renpy_mitch, renpy_will, renpy_johan, renpy_mel, renpy_felicity]
 # Formatting by adding quotation marks to dialogue and adding the 
 # character object at front, then inserting into relevant list.
-x = 0
-while x < len(sean):
-    speech = "\"" + sean[x] + "\""
-    renpy_sean.append(sean_object + " " + speech)
-    x = x+1        
 
-x=0
-while x<len(reader):
-    speech = "\"" + reader[x] + "\""
-    renpy_reader.append(reader_object + " " + speech)
-    x=x+1
 
-# Blue text format removes the asterisks, and changes the colour to blue (in-engine).
+
+i = 0
+while i < len(character_names):
+    sent = 0
+    person = list_character_names[i]
+    while sent < len(person):
+        speech = "\"" + person[sent] + "\""
+        char = list_renpy_names[i]
+        code = str(character_names[i]).lower() + "_object"
+        
+        char.append(object_codes[code] + " " + speech)
+        sent = sent+1  
+
+    i = i+1
+
+
+
+# x = 0
+# while x < len(sean):
+#     speech = "\"" + sean[x] + "\""
+#     renpy_sean.append(sean_object + " " + speech)
+#     x = x+1        
+
+# x=0
+# while x<len(reader):
+#     speech = "\"" + reader[x] + "\""
+#     renpy_reader.append(reader_object + " " + speech)
+#     x=x+1
+
+# Blue text format removes the quesiton mark, and changes the colour to blue (in-engine).
 x=0
 while x<len(blue_text):
-    cleaned_blue_text = blue_text[x].replace("*", "")
+    first_char = ((blue_text[x])[0])[0]
+    cleaned_blue_text = blue_text[x].replace(first_char, "")
     renpy_blue_text.append("\"{color=#2D70D6}" + remove_all_extra_spaces(cleaned_blue_text) + "{/color}\"")
     x=x+1
 
 x=0
 while x<len(narration):
-    renpy_narration.append("\"" + narration[x] + "\"")
+    
+    cleaned_narration = (narration[x])[2:]
+    renpy_narration.append("\"" + str(cleaned_narration) + "\"")
     x=x+1
+
+
+
+x=0
+while x<len(review):
+    renpy_review.append(">>>!!!REVIEW FORMATTING!!! \"" + review[x] + "\"")
+    x=x+1
+
+x=0
+while x<len(scene_transition):
+    cleaned_scene = (scene_transition[x])[3:]             ## review syntax
+    renpy_scene_transition.append("scene " + str(cleaned_scene).lower())
+    x=x+1
+
+x=0
+while x<len(expression_change):
+    line = str(expression_change[x]).lower()
+    line = line.replace("[","")
+    line = line.replace("]","")
+    values = line.split(": ")
+    name = values[0]
+    expression = values[1]
+    renpy_expression_change.append("show " + name + " " + expression)
+    x=x+1
+
 
 
 # Create an empty list for the final ordered script.
 final_script = []
 
 for line in chat_script:
-    if line.startswith("Sean:") == True:
-        final_script.append(renpy_sean.pop(0)) 
-        # Removes the first item from the dialogue list and adds it to the final_script list.
-    elif line.startswith("Reader:") == True:
-        final_script.append(renpy_reader.pop(0)) 
-    elif line.startswith("*") == True and line.endswith("*") == True:
+    
+    i = 0
+    while i < len(character_names):
+        name = character_names[i] + ":"
+        renpy_list_name = list_renpy_names[i]
+        if line.startswith(name) == True:
+            final_script.append(renpy_list_name.pop(0))
+        i = i+1
+
+    if line.startswith("?") == True:
         final_script.append(renpy_blue_text.pop(0))
-    else:
-        final_script.append(renpy_narration.pop(0)) 
+    elif line.startswith(">") == True or line.startswith("@@") == True:
+        final_script.append(renpy_review.pop(0))
+    elif line.startswith("[") == True:
+        final_script.append(renpy_expression_change.pop(0))
+    elif line.startswith("$$") == True:
+        final_script.append(renpy_scene_transition.pop(0))
+    elif line.startswith("#") == True:
+        final_script.append(renpy_narration.pop(0))
+
+
+
+    # if line.startswith("Sean:") == True:
+    #     final_script.append(renpy_sean.pop(0)) 
+    #     # Removes the first item from the dialogue list and adds it to the final_script list.
+    # elif line.startswith("Reader:") == True:
+    #     final_script.append(renpy_reader.pop(0)) 
+    # elif line.startswith("*") == True and line.endswith("*") == True:
+    #     final_script.append(renpy_blue_text.pop(0))
+    # else:
+    #     final_script.append(renpy_narration.pop(0)) 
     
 
 
