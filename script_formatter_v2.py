8b1495a6ae1664cf7ab46dd0ccb583ccdc2d7557
BuildTools <unconfigured@null.spigotmc.org>
2023-06-01 19:50:02 +1000

    finished script formatter v2

--- script_formatter_v2.py
+++ script_formatter_v2.py
@@ -24,52 +24,79 @@ formatting:
             - swap with person_id like 'r'
 """
 
-import re
-
-FILEPATH = r'Pineappleronpa-Report-Card\testing_formats_original.txt'
+FILEPATH = r'Pineappleronpa-Report-Card\testing_formats.txt'
 NEW_FILENAME = r"Pineappleronpa-Report-Card\formatted_script.txt"
 
 HIGHLIGHT_COLOR = "#2D70D6"
+# Set to False if you dont want
+# TRUE:     r "{color=#2D70D6}Why is he rubbing his stomach like that?{/color}"
+# FALSE:      "{color=#2D70D6}Why is he rubbing his stomach like that?{/color}"
+READER_SAYS_HIGHLIGHTS = True
 
 def process_file(source_file, dest_file):
-    with open(source_file) as source:
-        with open(dest_file) as dest:
+    print("Starting translation")
+    with open(source_file, encoding="UTF-8") as source:
+        with open(dest_file, "w", encoding="UTF-8") as dest:
             for line in source:
                 new_line = parse_line(line)
-                # dest.write(new_line)
+                print(new_line)
+                dest.write(new_line)
+    print("Finished")
 
 def parse_line(str: str):
     SYMBOLS = ["#","$$","?",">","["]
+    NAME_DICT = {
+        "Reader": "r",
+        "Luke" : "l",
+        "David" : "d",
+        "Sean" : "s",
+        "Julian" : "j",
+        "Riley" : "i",
+        "Mitch" : "m",
+        "Will" : "w",
+        "Johan" : "o",
+        "Mel" : "e",
+        "Felicity" : "f"
+    }
     indent = str.count("    ")
-    str = str.lstrip()
+    str = str.strip()
+
     comment = str.startswith("@@")
     if comment:
         str = str.replace("@@","",1)
+    comment = "@@" if comment else ""
+
     symbol = ""
     for s in SYMBOLS:
         if str.startswith(s):
             symbol = s
     text = str[len(symbol):].lstrip()
+    text = text.replace('"', "'") # Replace " with '
+
+    if text == "": return ""
 
     match symbol:
         case "#":
-            text = '"' + text + '"'
+            text = f"\"{comment}{text}\""
         case "$$":
-            text = "scene " + text.lower()
+            text = "scene " + text
         case "?":
-            text = f'"\{{color={HIGHLIGHT_COLOR}}}' + text + '{/color}"'
+            # See READER_SAYS_HIGHLIGHTS at top of file to change blue
+            text = f"{'r ' if READER_SAYS_HIGHLIGHTS else ''}\"{{color={HIGHLIGHT_COLOR}}}{comment}{text.split(':')[1].strip()}{{/color}}\""
         case ">":
-            text = "(Option)>" if indent == 1 else "(Nested)>"
+            text = f"(Option)>{text}" if indent == 1 else f"(Nested)>{text}"
             text = '"' + text + '":'
+            indent = 0
         case "[":
-            text.replace("]","")
-            text.replace(":","")
-            text = text.lower()
-    
-    return text
+            text = text.replace("]","")
+            text = text.replace(":","")
+            text = "show " + text.lower()
+        case _:
+            text = text.split(":")
+            text = f"{NAME_DICT[text[0]]} \"{comment}{text[1].lstrip()}\""
 
-
-
+    indent = "   " if indent else ""
+    return indent +  text + '\n'
 
 if __name__ == "__main__":
     process_file(FILEPATH, NEW_FILENAME)
